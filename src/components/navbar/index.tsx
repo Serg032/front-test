828aa04fdbff372b8a7bfdf45e3b0874020d6f91
Serg032 <sergioradigales@gmail.com>
2023-06-04 12:50:07 +0200

    navbar done

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import { AppBar, Grid, IconButton, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import '../../styles/navBar.css';
+import theme from '../../styles/theme';
+
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState(false);
+  const toggleDrawer = () => setState(!state);
+  const list: string[] = ['Home', 'Login', 'Personal Data'];
+
+  return (
+    <AppBar color="primary" sx={{ background: theme.palette.secondary.main }}>
+      <Grid container className="navbar-container">
+        <IconButton onClick={() => toggleDrawer()}>
+          <MenuIcon
+            color="primary"
+            sx={{ color: theme.palette.primary.light }}
+          />
+        </IconButton>
+        <Typography>{'invyo'.toUpperCase()}</Typography>
+      </Grid>
+      <Drawer open={state} onClose={() => toggleDrawer()}>
+        <Grid
+          className="drawer-links-container"
+          color="secondary"
+          sx={{ background: theme.palette.secondary.light }}
+        >
+          {list.map((item) => (
+            <Typography
+              style={{ padding: '1rem' }}
+              color={'primary'}
+              sx={{ color: theme.palette.primary.light }}
+            >
+              {item}
+            </Typography>
+          ))}
+        </Grid>
+      </Drawer>
+    </AppBar>
+  );
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#131FF2',
+      light: '#F2F2F2',
+      dark: '#303AF2',
+    },
+    secondary: {
+      main: '#777DF2',
+      light: '#A0A4F2',
+    },
+  },
+});
+
+export default theme;
